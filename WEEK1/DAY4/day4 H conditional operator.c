42a78527c393d1160cee7e42f05fdf86e5d4b840
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-13 15:54:48 +0530

    Update day4 H conditional operator.c

--- WEEK1/DAY4/day4 H conditional operator.c
+++ WEEK1/DAY4/day4 H conditional operator.c
@@ -1,6 +1,12 @@
+/*
+ALGORITHM:
+1.read two numbers from the user.
+2.find the biggest of those two numbers using conditional operator.
+3.read a number and find whether it is even or odd.
+4.find whether the number is positive or negative, and print it.
+*/
 // Online C compiler to run C program online
 #include <stdio.h>
-
 int main() {
     int a,b,n;
     printf("enter two numbers:");
