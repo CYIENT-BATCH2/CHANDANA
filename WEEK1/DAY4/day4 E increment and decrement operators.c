1f45d6cf26c117b88fe11872da16a49c9b51880d
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-13 15:43:53 +0530

    Update day4 E increment and decrement operators.c

--- WEEK1/DAY4/day4 E increment and decrement operators.c
+++ WEEK1/DAY4/day4 E increment and decrement operators.c
@@ -1,3 +1,10 @@
+/*
+ALGORITHM:
+1.two variables are initialized at compile time.
+2.evaluate given expression using increment and decrement operators.
+3.store the final value in variable 'k'.
+*/
+
 // Online C compiler to run C program online
 #include <stdio.h>
 int main() {
