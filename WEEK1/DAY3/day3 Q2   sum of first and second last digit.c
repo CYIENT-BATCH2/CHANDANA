b92151daac5e438c01e12b07e04580de2eed1605
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-13 14:27:53 +0530

    Update day3 Q2   sum of first and second last digit.c

--- WEEK1/DAY3/day3 Q2   sum of first and second last digit.c
+++ WEEK1/DAY3/day3 Q2   sum of first and second last digit.c
@@ -1,3 +1,17 @@
+/*
+ALGORITHM-
+
+1.read  no.. from user 
+2.seperating digit by modulus.
+3.adding that digit with multiplication of 10 to previous.
+4.storing in to variable ,
+5. and printing .
+*/
+
+
+
+
+
 #include <stdio.h>
 
 int main() 
