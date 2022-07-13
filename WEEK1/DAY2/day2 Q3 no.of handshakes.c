692deb38ff6df7e464f0b45509ab867253041ba6
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-13 15:13:26 +0530

    Update day2 Q3 no.of handshakes.c

--- WEEK1/DAY2/day2 Q3 no.of handshakes.c
+++ WEEK1/DAY2/day2 Q3 no.of handshakes.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+/*
+ALGORITHM:
+1.read the number no.of students from the user.
+2.calculate the number of handshakes done in the classes.
+3.print the total number of handshakes. 
+*/
 
+#include <stdio.h>
 int main() {
   int n;
   printf("enter no.of students:");
