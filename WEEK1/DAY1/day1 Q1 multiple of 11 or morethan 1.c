c0beabca9731bddd1bffc0946ff1c4c4248c51ab
chandana326 <108974070+chandana326@users.noreply.github.com>
2022-07-13 14:45:03 +0530

    Update day1 Q1 multiple of 11 or morethan 1.c

--- WEEK1/DAY1/day1 Q1 multiple of 11 or morethan 1.c
+++ WEEK1/DAY1/day1 Q1 multiple of 11 or morethan 1.c
@@ -1,3 +1,10 @@
+/*
+algorithm:
+1.read a number as input from the user
+2.check whether the number is multiple of 11 or morethan one of multiple of 11
+3.if it is multiple,print as SPECIAL.
+*/
+
 #include <stdio.h>
 int main() {
     int n;
